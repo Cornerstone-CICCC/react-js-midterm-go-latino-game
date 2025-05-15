bafafa10e13da43baea8086d42d84095ec535c03
Majito Brinkitos <mcvef2013@gmail.com>
2025-05-15 12:19:48 -0700

    signup ready

--- frontend/src/components/Signup/Signup.tsx
+++ frontend/src/components/Signup/Signup.tsx
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom'
+
 function Signup() {
     return(
-        <div className="flex flex-row justify-between gap-2 border border-black mx-50 bg-[#e3e3e3] rounded-4xl mb-8 p-8">
-            <div className="flex flex-col border border-black p-8">
+        <div className="flex flex-row justify-between gap-2 border border-black mx-50 bg-[#e3e3e3] rounded-4xl mb-8">
+            <div className="flex flex-col m-8 px-8">
                 <div className="logo flex flex-row justify-center">
-                    <img src="vite.svg" alt="logo" />
+                    <img className='w-25' src="logoSignUp.svg" alt="logo" />
                 </div> 
                 <div className="signupTitle flex flex-col items-center">
                     <p className="header text-[40px]">Sign up</p>
@@ -12,24 +14,70 @@ function Signup() {
                 {/*Form */}
                 <div className="userForm">
                     <form action="">
-                        {/*Full Name */}
+                        {/*First Name */}
                         <div className="flex justify-center p-2">
                         <label htmlFor="">
                             <input 
                             className="border border-black 
                             rounded-lg p-2" 
                             type="text" 
-                            placeholder="Name"/>
+                            placeholder="First Name"/>
                         </label>
                         </div>
-                        {/* */}
+                        {/*Last Name */}
+                        <div className="flex justify-center p-2">
+                        <label htmlFor="">
+                            <input 
+                            className="border border-black 
+                            rounded-lg p-2" 
+                            type="text" 
+                            placeholder="Last Name"/>
+                        </label>
+                        </div>
+                        {/*Age */}
+                        <div className="flex justify-center p-2">
+                        <label htmlFor="">
+                            <input 
+                            className="border border-black 
+                            rounded-lg p-2" 
+                            type="number" 
+                            placeholder="Age"/>
+                        </label>
+                        </div>
+                        {/*Password */}
+                        <div className="flex justify-center p-2">
+                        <label htmlFor="">
+                            <input 
+                            className="border border-black 
+                            rounded-lg p-2" 
+                            type="password" 
+                            placeholder="Password"/>
+                        </label>
+                        </div>
+                        {/*Email */}
+                        <div className="flex justify-center p-2">
+                        <label htmlFor="">
+                            <input 
+                            className="border border-black 
+                            rounded-lg p-2" 
+                            type="email" 
+                            placeholder="Email"/>
+                        </label>
+                        </div>
+                        {/*Sign up Button */}
+                        <div className="flex flex-row justify-center">
+                            <button className="header text-2xl bg-[#f95959] p-2 rounded-s-lg">
+                            <Link to="/signup">Signup</Link>
+                            </button>
+                        </div>
                     </form>
                 </div>
             </div>
-            <div className="flex flex-col">
-                <img className="border rounded-lg" src="/images/model2.png" alt="model" />
+            <div className="flex flex-col justify-center w-150">
+                <img className="border h-full" src="/images/signup-bg.png" alt="model" />
             </div>
         </div>
+        
     );
 }
 
