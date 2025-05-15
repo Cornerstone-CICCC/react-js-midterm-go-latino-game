242dac8989d3d18c71323853fc27903964b46eab
Julio-SC <jhver2001@gmail.com>
2025-05-15 12:26:16 -0700

    stripe changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,10 @@ import Catalog from './components/Catalog/Catalog'
 import Main from './components/Main/Main'
 import Login from './components/Login/Login'
 import Signup from './components/Signup/Signup'
+import Success from '../src/components/Stripe/Sucess';
+import Cancel from '../src/components/Stripe/Cancel';
+import Cart from './components/Cart/CartPage';
+
 import './App.css'
 
 function App(){
@@ -16,6 +20,10 @@ function App(){
           <Route index element={<Main />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+           <Route path="/success" element={<Success />} />
+          <Route path="/cart" element={<Cart />} />
+  <Route path="/cancel" element={<Cancel />} />
+
         </Route>
       </Routes>
     </BrowserRouter>
