6b24dd5669689c69346abe969cce37652509b00e
Majito Brinkitos <mcvef2013@gmail.com>
2025-05-16 12:09:24 -0700

    admincatalog

--- frontend/src/components/Admin/AdminCatalog.tsx
+++ frontend/src/components/Admin/AdminCatalog.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react'
+
 function AdminCatalog(){
-    const products =[
+    const [products, setProducts] = useState([
         { id: 1,
             name: "Skinny Jeans", 
             price: 22.99, 
@@ -37,7 +39,34 @@ function AdminCatalog(){
             category: "Women",
             stock:""
         }
-    ]
+    ]);
+
+    //addNewProduct
+    const addNewProduct = () => {
+        const newProduct = {
+            id: products.length + 1,
+            name: "New Product",
+            price: 0.00,
+            description: "",
+            size: "",
+            image: "",
+            category: "",
+            stock:"In Stock",
+        };
+        setProducts([...products, newProduct]);
+    };
+
+    //editProduct
+    const editProduct = (id) => {
+        setProducts(products.map(product => 
+            product.id === id ? { ...product, name: "Product has been updated" } : product
+        ));
+    };
+
+    //deleteProduct
+    const deleteProduct = (id) => {
+        setProducts(products.filter(product => product.id !== id));
+    };
 
     return(
         <div className="p-8 bg-[#e3e3e3] text-[#233142] m-8 rounded-md">
@@ -50,7 +79,7 @@ function AdminCatalog(){
 
             {/*Add Product Button */}
             <button
-            onClick={() => addNewProduct(product)}
+            onClick={addNewProduct}
             className="
             flex
             justify-center
