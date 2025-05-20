3d7836ef3bc864626ac0d5b69ee6e0ad50b9bce1
Majito Brinkitos <mcvef2013@gmail.com>
2025-05-19 17:00:44 -0700

    responsive design

--- frontend/src/components/Main/Main.tsx
+++ frontend/src/components/Main/Main.tsx
@@ -7,20 +7,20 @@ const Main = () => {
                 <p className="flex justify-center text-[#e3e3e3] intro">Where heritage is reinvented</p>
             </div>
             <div className="flex flex-row gap-6 p-6">
-                <div className="flex flex-col">
-                    <img className="rounded-s-full w-full max-w-80 h-full" src="/images/model1.png" alt="model 1" />
+                <div className="flex justify-center items-center">
+                    <img className="image-container rounded-s-full w-full max-w-80 h-full" src="/images/model1.png" alt="model 1" />
                 </div>
-                <div className="flex flex-col">
-                    <img className="rounded-t-full  w-full max-w-80 h-full" src="/images/model3.png" alt="model 1" />
+                <div className="flex  justify-center items-center">
+                    <img className="image-container rounded-t-full  w-full max-w-80 h-full" src="/images/model3.png" alt="model 3" />
                 </div>
-                <div className="flex flex-col">
-                    <img className="rounded-b-full  w-full max-w-80 h-full" src="/images/model2.png" alt="model 1" />
+                <div className="flex justify-center items-center">
+                    <img className="image-container rounded-b-full  w-full max-w-80 h-full" src="/images/model2.png" alt="model 2" />
                 </div>
-                <div className="flex flex-col">
-                    <img className="rounded-e-full  w-full max-w-80 h-full" src="/images/model4.png" alt="model 1" />
+                <div className="flex justify-center items-center">
+                    <img className="image-container rounded-e-full  w-full max-w-80 h-full" src="/images/model4.png" alt="model 4" />
                 </div>
             </div>
-            
+
         </div>
     );
 }
